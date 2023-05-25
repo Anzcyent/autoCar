8c0c37cce64c8943601108522a06a8a9c89cf7bd
Cihan <ozcancihan714@gmail.com>
2023-05-25 14:45:48 +0300

    v1.0.2 contact us change

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,17 +4,20 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <section className="contact">
-      <h3 className="animate__animated animate__fadeIn">AutoCar</h3>
+      <h3 className="animate__animated animate__fadeIn">
+        Contact Us For More Information
+      </h3>
 
       <div className="contact-info animate__animated animate__zoomIn">
         <p>
           <span style={{ fontWeight: "bold" }}>Address:</span> 1234 st. Türkiye
         </p>
         <p>
-          <span style={{ fontWeight: "bold" }}>Phone:</span> +90 123 456 7890 
+          <span style={{ fontWeight: "bold" }}>Phone:</span> +90 123 456 7890
         </p>
         <p>
-          <span style={{ fontWeight: "bold" }}>Email:</span> ozcancihan714@gmail.com 
+          <span style={{ fontWeight: "bold" }}>Email:</span>{" "}
+          ozcancihan714@gmail.com
         </p>
       </div>
 
